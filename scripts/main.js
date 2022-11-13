e8222ea05828743bc6974931c16956b97cf40e8f
Niko <nikodem.pawlowski@gmail.com>
2022-11-13 19:28:37 +0000

    Draw Circle
    Working on drawing circle functions.

--- scripts/main.js
+++ scripts/main.js
@@ -2,92 +2,78 @@ var whiteboard = document.getElementById("whiteboard");
 var context = whiteboard.getContext("2d");
 var colourButton = document.getElementById("PencilColour");
 var shapeButton = document.getElementById("SelectShape");
+var drawMethodSelected = shapeButton.value;
 
 whiteboard.height = window.innerHeight;
 whiteboard.width = window.innerWidth;
 
 window.onload = function () {
-  function changeShape() {
-    var shape = shapeButton.value;
-    console.log(shape);
-    if (shape == "Line") {
-      drawWithPencil();
-      // let x = true;
-    }
-    if (shape == "Circle") {
-      drawWithCircle();
-    }
-    // if (shape == "Rectangle") {
-
-    // }
-  }
-
-  // drawWithPencil();
-
   function changeColour() {
     var colour = colourButton.value;
     context.strokeStyle = colour;
   }
 
-  // Listener for drawing tools
   colourButton.addEventListener("change", changeColour);
-  shapeButton.addEventListener("change", changeShape);
+  shapeButton.addEventListener("change", () => {
+    draw();
+  });
 };
 
-function drawWithCircle() {
-  console.log("Draw circle ");
+// Variables
+let pencil = false;
+var canvasX, canvasY;
+
+function draw() {
+  drawMethodSelected = shapeButton.value;
+  if (drawMethodSelected == "Line") {
+    whiteboard.addEventListener("mousedown", startPositionPencil);
+    whiteboard.addEventListener("mouseup", finishPositionPencil);
+    whiteboard.addEventListener("mousemove", drawPencil);
+  } else if (drawMethodSelected == "Circle") {
+    disablePencil();
+
+    whiteboard.addEventListener("mousedown", startPosition);
+    whiteboard.addEventListener("mouseup", finishPosition);
+    whiteboard.addEventListener("mousemove", drawPencil);
+  } else if (drawMethodSelected == "Rectangle") {
+    disablePencil();
+  }
 }
 
-function drawWithPencil() {
-  // Variables
-  let pencil = false;
-  var canvasX, canvasY;
-
-  function startPosition(e) {
-    pencil = true;
-
-    // Draw dots
-    draw(e);
-  }
-
-  function finishPosition() {
-    pencil = false;
-
-    // Reset path to draw new lines without connecting to old line
-    context.beginPath();
-  }
-
-  // Draw on whiteboard
-  function draw(e) {
-    if (!pencil) return;
-    context.linewidth = 10;
-    context.lineCap = "round";
-
-    // Set mouse cursor pointer according to whiteboard
-    canvasX = e.pageX - whiteboard.offsetLeft;
-    canvasY = e.pageY - whiteboard.offsetTop;
-    // context.lineTo(e.clientX, e.clientY);
-    context.lineTo(canvasX, canvasY);
-    context.stroke();
-    context.beginPath();
-    // context.moveTo(e.clientX, e.clientY);
-    context.moveTo(canvasX, canvasY);
-
-    //context.strokeStyle = "red"; - change colour of line
-  }
-
-  // Mouse listener for drawing
-  whiteboard.addEventListener("mousedown", startPosition);
-  whiteboard.addEventListener("mouseup", finishPosition);
-  whiteboard.addEventListener("mousemove", draw);
-
-  // changeSize();
+function disablePencil() {
+  whiteboard.removeEventListener("mousedown", startPositionPencil);
+  whiteboard.removeEventListener("mouseup", finishPositionPencil);
+  whiteboard.removeEventListener("mousemove", drawPencil);
 }
 
-// // Resize whiteboard application
-// window.addEventListener('resize', changeSize);
+// Drawing with pencil
+function startPositionPencil(e) {
+  pencil = true;
 
-// function changeSize() {
-//     whiteboard.height = window.innerHeight;
-//     whiteboard.width = window.innerWidth;
-// }
+  // Draw dots
+  drawPencil(e);
+}
+
+function finishPositionPencil() {
+  pencil = false;
+
+  // Reset path to draw new lines without connecting to old line
+  context.beginPath();
+}
+
+// Draw on whiteboard
+function drawPencil(e) {
+  if (!pencil) return;
+  context.linewidth = 10;
+  context.lineCap = "round";
+
+  // Set mouse cursor pointer according to whiteboard
+  canvasX = e.pageX - whiteboard.offsetLeft;
+  canvasY = e.pageY - whiteboard.offsetTop;
+  // context.lineTo(e.clientX, e.clientY);
+  context.lineTo(canvasX, canvasY);
+  context.stroke();
+  context.beginPath();
+  // context.moveTo(e.clientX, e.clientY);
+  context.moveTo(canvasX, canvasY);
+}
--- scripts/test.js
+++ scripts/test.js
@@ -2,41 +2,12 @@ var whiteboard = document.getElementById("whiteboard");
 var context = whiteboard.getContext("2d");
 var colourButton = document.getElementById("PencilColour");
 var shapeButton = document.getElementById("SelectShape");
+var drawMethodSelected = shapeButton.value;
 
 whiteboard.height = window.innerHeight;
 whiteboard.width = window.innerWidth;
 
-let pencilEnabled = true;
-let shape = shapeButton.value;
-
 window.onload = function () {
-  // var x = 1;
-  // if (x == 1) {
-  //     pencilEnabled = false;
-  // }
-
-  // console.log(shape);
-
-  // if (shape == "Line") {
-  //     // pencilEnabled = true;
-  //     console.log("Line");
-  // }
-  // if (shape == "Circle") {
-  //     // pencilEnabled = false;
-  //     console.log("Circle");
-  // }
-
-  // function changeShape() {
-  //     let shape = shapeButton.value;
-  //     console.log(shape);
-  //     if (shape == "Circle") {
-  //         pencilEnabled = false;
-  //     }
-
-  // }
-
-  drawWithPencil();
-
   function changeColour() {
     var colour = colourButton.value;
     context.strokeStyle = colour;
@@ -44,69 +15,118 @@ window.onload = function () {
 
   colourButton.addEventListener("change", changeColour);
   shapeButton.addEventListener("change", () => {
-    shape = shapeButton.value;
-    drawChecker();
+    draw();
   });
-
-  function drawChecker() {
-    if (shape == "Line") {
-      // pencilEnabled = true;
-      // console.log(shape);
-      window.addEventListener("load", drawWithPencil, true);
-    }
-    if (shape == "Circle") {
-      pencilEnabled = false;
-      console.log(shape);
-    }
-    if (shape == "Rectangle") {
-      pencilEnabled = false;
-      console.log(shape);
-    }
-  }
 };
 
-function drawWithPencil() {
-  if (pencilEnabled) {
-    // Variables
-    let pencil = false;
-    var canvasX, canvasY;
+// Variables
+let pencil = false;
+var canvasX, canvasY;
 
-    function startPosition(e) {
-      pencil = true;
+function draw() {
+  drawMethodSelected = shapeButton.value;
+  if (drawMethodSelected == "Line") {
+    whiteboard.addEventListener("mousedown", startPositionPencil);
+    whiteboard.addEventListener("mouseup", finishPositionPencil);
+    whiteboard.addEventListener("mousemove", drawPencil);
+  } else if (drawMethodSelected == "Circle") {
+    disablePencil();
 
-      // Draw dots
-      draw(e);
-    }
-
-    function finishPosition() {
-      pencil = false;
-
-      // Reset path to draw new lines without connecting to old line
-      context.beginPath();
-    }
-
-    // Draw on whiteboard
-    function draw(e) {
-      if (!pencil) return;
-      context.linewidth = 10;
-      context.lineCap = "round";
-
-      // Set mouse cursor pointer according to whiteboard
-      canvasX = e.pageX - whiteboard.offsetLeft;
-      canvasY = e.pageY - whiteboard.offsetTop;
-      // context.lineTo(e.clientX, e.clientY);
-      context.lineTo(canvasX, canvasY);
-      context.stroke();
-      context.beginPath();
-      // context.moveTo(e.clientX, e.clientY);
-      context.moveTo(canvasX, canvasY);
-
-      //context.strokeStyle = "red"; - change colour of line
-    }
-
-    // Mouse listener for drawing
-    whiteboard.addEventListener("mousedown", startPosition);
-    whiteboard.addEventListener("mouseup", finishPosition);
-    whiteboard.addEventListener("mousemove", draw);
+    whiteboard.addEventListener("mousedown", startPositionCircle);
+    whiteboard.addEventListener("mouseup", finishPositionCircle);
+    whiteboard.addEventListener("mousemove", mouseMove);
+    whiteboard.addEventListener("mouseout", mouseOut);
+  } else if (drawMethodSelected == "Rectangle") {
+    disablePencil();
   }
+}
+
+function disablePencil() {
+  whiteboard.removeEventListener("mousedown", startPositionPencil);
+  whiteboard.removeEventListener("mouseup", finishPositionPencil);
+  whiteboard.removeEventListener("mousemove", drawPencil);
+}
+
+// Drawing with pencil
+function startPositionPencil(e) {
+  pencil = true;
+
+  // Draw dots
+  drawPencil(e);
+}
+
+function finishPositionPencil() {
+  pencil = false;
+
+  // Reset path to draw new lines without connecting to old line
+  context.beginPath();
+}
+
+// Draw on whiteboard
+function drawPencil(e) {
+  if (!pencil) return;
+  context.linewidth = 10;
+  context.lineCap = "round";
+
+  // Set mouse cursor pointer according to whiteboard
+  canvasX = e.pageX - whiteboard.offsetLeft;
+  canvasY = e.pageY - whiteboard.offsetTop;
+  // context.lineTo(e.clientX, e.clientY);
+  context.lineTo(canvasX, canvasY);
+  context.stroke();
+  context.beginPath();
+  // context.moveTo(e.clientX, e.clientY);
+  context.moveTo(canvasX, canvasY);
+}
+
+// Drawing with circle
+
+function startPositionCircle(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  startX = e.pageX - whiteboard.offsetLeft;
+  startY = e.pageY - whiteboard.offsetTop;
+  pencil = true;
+}
+
+function finishPositionCircle(e) {
+  if (!pencil) return;
+  e.preventDefault();
+  e.stopPropagation();
+  pencil = false;
+}
+
+function mouseOut(e) {
+  if (!pencil) return;
+  e.preventDefault();
+  e.stopPropagation();
+  pencil = false;
+}
+
+function mouseMove(e) {
+  if (!pencil) return;
+  e.preventDefault();
+  e.stopPropagation();
+
+  canvasX = e.pageX - whiteboard.offsetLeft;
+  canvasY = e.pageY - whiteboard.offsetTop;
+
+  drawCircle(canvasX, canvasY);
+}
+
+function drawCircle(x, y) {
+  context.clearRect(0, 0, whiteboard.width, whiteboard.height);
+  context.beginPath();
+  context.moveTo(startX, startY + (y - startY) / 2);
+  context.bezierCurveTo(
+    startX,
+    startY,
+    x,
+    startY,
+    x,
+    startY + (y - startY) / 2
+  );
+  context.bezierCurveTo(x, y, startX, y, startX, startY + (y - startY) / 2);
+  context.closePath();
+  context.stroke();
 }
