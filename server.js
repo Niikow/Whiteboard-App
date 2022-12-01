9fea19fd622c215cbdb1ad4039711f15fd0c541e
Ali Coskun <acoskunbiz@gmail.com>
2022-12-01 22:05:21 +0000

    testing docker

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 async function main() {
-    const pubClient = redis.createClient({ host: process.env.REDIS_ENDPOINT, port: 6379 });
+    const pubClient = redis.createClient({ url: "redis://localhost:6379" });
     const subClient = pubClient.duplicate();
     await Promise.all([pubClient.connect(), subClient.connect()]);
     io.adapter(adapter.createAdapter(pubClient, subClient));
@@ -42,7 +42,7 @@ async function main() {
 
     app.use(express.static("public"));
 
-    let PORT = process.env.PORT || 3000;
+    var PORT = process.env.PORT || 3000;
     http.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 
